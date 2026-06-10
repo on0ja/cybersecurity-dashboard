65b2d8e16d5d482c6097a9355994a05d5d0c5626
on0ja <davidbernard697@gmail.com>
2026-06-11 00:55:32 +0100

    Add files via upload

---
+++ SEN 182 GROUP PROJECT/script.js
@@ -0,0 +1,7 @@
+function openModal(){
+  document.getElementById("alertModal").style.display = "block";
+}
+
+function closeModal(){
+  document.getElementById("alertModal").style.display = "none";
+}
